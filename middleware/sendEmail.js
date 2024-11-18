b05c949abc7ce10aac62b36edd10beb881610cd4
Celistine02 <148200763+Celistine02@users.noreply.github.com>
2024-11-18 11:09:18 +0200

    Update sendEmail.js

--- middleware/sendEmail.js
+++ middleware/sendEmail.js
@@ -1,21 +1,29 @@
-const transporter = require("./../../config/emailConfig");
 
-require("dotenv").config();
 
-const sendEmail = async (to, subject, htmlContent) => {
-  const mailOptions = {
-    from: `"Cloud Campus" <${process.env.EMAIL}>`, // Sender address loaded from environment variables
-    to, // Recipient address
-    subject, // Subject line
-    html: htmlContent, // HTML body content
+
+
+const net = require("net");
+
+const checkSmtpConnection = () => {
+  const options = {
+    port: 465,
+    host: "smtp.gmail.com",
+    timeout: 5000,
   };
 
-  try {
-    await transporter.sendMail(mailOptions);
-    console.log("Email sent successfully");
-  } catch (error) {
-    console.error("Error sending email:", error);
-  }
+  const client = net.createConnection(options, () => {
+    console.log("Connected to SMTP server");
+    client.end();
+  });
+
+  client.on("error", (err) => {
+    console.error("Connection error:", err);
+  });
+
+  client.on("timeout", () => {
+    console.error("Connection timed out");
+    client.end();
+  });
 };
 
-module.exports = sendEmail;
+module.exports = checkSmtpConnection;
